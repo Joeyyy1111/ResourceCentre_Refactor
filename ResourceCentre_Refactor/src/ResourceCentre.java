7000b9364aa245422e007a1b1971ae945d5eac8e
21023096 <21023096@DESKTOP-CHNVKED>
2022-07-27 09:52:55 +0800

    Yong xing modified retrieveAllCamcorder

--- ResourceCentre_Refactor/src/ResourceCentre.java
+++ ResourceCentre_Refactor/src/ResourceCentre.java
@@ -151,7 +151,7 @@ public class ResourceCentre {
 			output += String.format("%-84s\n", camcorderList.get(i).toString());
 		}
 		return output;
-		//Yong xing
+		//Yong xing 
 	}
 	
 	public static void viewAllCamcorder(ArrayList<Camcorder> camcorderList) {
